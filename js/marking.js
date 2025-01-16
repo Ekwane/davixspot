ebcb28828f451a86dac49a5bcb27a41c54dc0e66
Ekwane <58188464+Ekwane@users.noreply.github.com>
2025-01-16 14:45:31 +0100

    Most recent project update

---
+++ js/marking.js
@@ -0,0 +1,33 @@
+var i;
+var input1; //input2, input3, input4, input5, input6, input7, input8, input9, input10;
+var sel1 = document.getElementsByName('sel1'); 
+// sel2 = document.getElementsByName('sel2'),
+// sel3 = document.getElementsByName('sel3'),
+// sel4 = document.getElementsByName('sel4'),
+// sel5 = document.getElementsByName('sel5'),
+// sel6 = document.getElementsByName('sel6'),
+// sel7 = document.getElementsByName('sel7'),
+// sel8 = document.getElementsByName('sel8'),
+// sel9 = document.getElementsByName('sel9'),
+// sel10 = document.getElementsByName('sel10');
+		 function testiii() { 
+		 	for (i=0; i < sel1.length; i++) {
+		 	if(sel1[i].value==answersAGRIC2020[0]) alert('correct');
+		 } 
+		}
+
+		 	// if(input1!=null) {
+		 	// 	for(i=0;i<sel2.length; i++) {
+		 	// 		if(sel2[i].value==responses[1]) input2 = sel2[i];
+		 	// 		if(input2!=null) {
+		 	// 			for(i=0;i<sel3.length; i++) {
+		 	// 				if(sel3[i].value==responses[2]) input3 = sel3[i];
+		 	// 				if(input3!=null) {
+		 	// 					for(i=0;i<sel4.length; i++) {
+		 	// 						if(sel4[i].value==responses[3]) input4 = sel4[i]
+		 	// 					}
+		 	// 				}
+		 	// 			}
+		 	// 		}
+		 	// 	} 
+		 	// }
